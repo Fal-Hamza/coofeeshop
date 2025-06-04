c3ed38be04046d4f8e84edd7d443337e0d9ba2b8
fal hamza <fal.hamza98@gmail.com>
2025-06-04 02:33:28 +0200

    nav trigger btn Animation

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,12 +7,16 @@ import { useState } from "react";
 import Nav from "./Nav";
 
 const Header = () => {
+  const [navActive, setNavActive] = useState(false);
   return (
     <header className="absolute top-[48px] left-0 right-0 z-[60]">
-      <div className="container ma-auto">
-        <div>
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
           {/* Logo */}
-          <Link href="/">
+          <Link
+            href="/"
+            className="relative w-[100px] h-[40px] z-[60] xl:w-[120px] xl:h-[40px]"
+          >
             <Image
               src="/assets/logo.svg"
               fill
@@ -20,9 +24,34 @@ const Header = () => {
               className="object-contain"
             />
           </Link>
-          Header
+          {/* nav trigger btn */}
+          <button
+            onClick={() => setNavActive(!navActive)}
+            className="w-8 h-6 text-accent relative flex items-center justify-center z-[60] outline-none"
+          >
+            {/* [1] */}
+            <span
+              className={`w-full h-[1.5px] bg-current absolute left-0 will-change-transform transition-transform duration-300 ${
+                navActive ? "top-1/2 rotate-45" : "top-0 translate-y-0"
+              }`}
+            ></span>
+            {/* [2] */}
+            <span
+              className={`w-full h-[1.5px] bg-current absolute left-0 will-change-transform transition-transform duration-300 ${
+                navActive ? "opacity-0" : "top-1/2"
+              }  `}
+            ></span>
+            {/* [3] */}
+            <span
+              className={`w-full h-[1.5px] bg-current absolute left-0 will-change-transform transition-transform duration-300 ${
+                navActive ? " top-1/2 -rotate-45" : "bottom-0 translate-y-0"
+              }`}
+            ></span>
+          </button>
         </div>
       </div>
+      {/* nav */}
+      <AnimatePresence mode="wait">{navActive && <Nav />}</AnimatePresence>
     </header>
   );
 };
